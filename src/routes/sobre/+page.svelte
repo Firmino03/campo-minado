199f13e219e017c7c566a769f29bd9a0ae250ee1
Firmino <karolaynefirmino27@gmail.com>
2025-12-04 13:32:28 -0300

    Alteração no sobre

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -20,12 +20,6 @@
 
 <img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
 
-<p>
-    Autor: Allan Diego Silva Lima
-</p>
-<p>
-  <a href="allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
-</p>
 
 >>>>>>> e62091055934b1453e6567c56b3db20893683e4a
 <a class="menu" href="/">Voltar ao Menu</a>
