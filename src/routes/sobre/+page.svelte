aae93897dcf46248b318f6142878b5d2aec3bf32
Firmino <karolaynefirmino27@gmail.com>
2025-12-04 13:00:28 -0300

    Editando o Sobre

---
+++ src/routes/sobre/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Sobre</h1>
+
+<p>Este projeto é uma implementação do clássico jogo Campo Minado, conhecido por desafiar a lógica e a atenção do jogador. O objetivo é revelar todas as casas do tabuleiro que não contêm bombas, usando apenas os números exibidos como pistas — cada número indica quantas bombas existem ao redor daquela posição.</p>
+  
+  <p>Desenvolvedores: </p>
+
+<p> Karolayne Firmino </p>
+<p> Carla Severina  </p>
+<p> Ana Claúdia </p>
+<p> Maria Eloisa </p>
+<p> Amanda Gabrielly </p>
+<img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
+</p>
+
+  <a href="https://github.com/Firmino03/campo-minado.git">GitHub</a>
+
+<a class="menu" href="/">Voltar ao Menu</a>
