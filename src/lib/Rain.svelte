bcfc56f3dc55b08a809f2b82584ba21cccb4283a
Firmino <karolaynefirmino27@gmail.com>
2025-12-09 13:34:54 -0300

    Implementação de efeito cascata

---
+++ src/lib/Rain.svelte
@@ -0,0 +1,26 @@
+<script>
+    // Cria números caindo aleatoriamente
+    export let amount = 40;
+
+    let items = Array.from({ length: amount }, () => ({
+        left: Math.random() * 100,      // posição horizontal aleatória
+        delay: Math.random() * 3,       // atraso aleatório na animação
+        dur: 3 + Math.random() * 2,     // duração da animação
+        num: Math.ceil(Math.random() * 5) // número 1-5
+    }));
+</script>
+
+<div class="rain-container">
+    {#each items as item}
+        <span
+            class="drop"
+            style="
+                left: {item.left}%;
+                animation-delay: {item.delay}s;
+                animation-duration: {item.dur}s;
+            "
+        >
+            {item.num}
+        </span>
+    {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     let tituloDoJogo: string = "Campo-Minado";
+    import Rain from '$lib/Rain.svelte';
 </script>
 
-<h1>
-    {tituloDoJogo}
-</h1>
+<main class="home-container">
+    <Rain amount={50} />
 
-<br />
-<a class="menu" href="/jogar">Jogar</a>
-<br />
-<a class="menu" href="/sobre">Sobre</a>
+    <h1 class="title">Campo Minado</h1>
+
+<a class="menu" href="/jogar">Iniciar Jogo</a>
+    <a class="menu" href="/sobre">Sobre</a>
+</main>
